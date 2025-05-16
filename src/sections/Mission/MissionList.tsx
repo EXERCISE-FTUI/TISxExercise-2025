b2472edf4e1c9ba4db3465384eca011c3d346f8f
nardaiv <nardaikiru@gmail.com>
2025-05-16 21:09:30 +0700

    updating style in MissionList

--- src/sections/Mission/MissionList.tsx
+++ src/sections/Mission/MissionList.tsx
@@ -7,18 +7,16 @@ type MissionListProps = {
 
 const MissionList = ({ order, point }: MissionListProps) => {
   return (
-    <div className="relative h-30">
-      <div className="absolute -left-5 top-0 h-full bg-skyBlue/75 rounded-full aspect-square flex items-center">
-        <div className="text-7xl bg-white font-bold mx-auto text-center w-[80%] aspect-square rounded-full my-auto italic pe-[8px] textstroke-white text-navyPurple flex items-center">
-          <p className="w-full">{order}</p>
+    <div className="relative flex h-30">
+      <div className="absolute -left-5 top-0 h-full bg-linear-to-t from-skyBlue/75 to-softBlue/75 rounded-full aspect-square flex items-center">
+        <div className="text-7xl bg-white font-bold mx-auto text-center w-[80%] aspect-square rounded-full my-auto italic pe-[8px] text-navyPurple flex items-center">
+          <p className="w-full textstroke-white">{order}</p>
         </div>
       </div>
 
-      <div className="w-full flex items-center justify-center h-full">
-        <p className="text-white h-[60%] w-full bg-navyPurple font-semibold p-3 ps-28 rounded-2xl text-sm my-auto">
-          {point}
-        </p>
-      </div>
+      <div className="w-full flex h-8/12 my-auto bg-navyPurple font-semibold ps-28 p-3  rounded-3xl items-center shadow-softBlue shadow-md">
+        <p className="text-white text-sm">{point}</p>
+          </div>
     </div>
   );
 };
