3473082b0c23c486213181606bbd43cdc67cba34
davidalexander2411 <davidalexander2411@gmail.com>
2025-05-18 02:15:30 +0700

    Added second gallery and title

--- src/sections/Gallery/Gallery.tsx
+++ src/sections/Gallery/Gallery.tsx
@@ -5,14 +5,21 @@ import useEmblaCarousel from 'embla-carousel-react'
 import Image from 'next/image'
 
 export function Gallery() {
-  const [emblaRef] = useEmblaCarousel({
+  const [emblaRef1] = useEmblaCarousel({
     dragFree: true,
     loop: true,
     align: 'center',
     slidesToScroll: 1
   })
 
-  const images = [
+  const [emblaRef2] = useEmblaCarousel({
+    dragFree: true,
+    loop: true,
+    align: 'center',
+    slidesToScroll: 1
+  })
+
+  const images1 = [
     '/assets/Gallery/images/IMG_1418.png',
     '/assets/Gallery/images/IMG_1411.png',
     '/assets/Gallery/images/IMG_1410.png',
@@ -23,6 +30,19 @@ export function Gallery() {
     '/assets/Gallery/images/IMG_1419.png',
     '/assets/Gallery/images/IMG_1413.png',
     '/assets/Gallery/images/IMG_1414.png',
+  ]
+
+  const images2 = [
+    '/assets/Gallery/images/IMG_1389.png',
+    '/assets/Gallery/images/IMG_1391.png',
+    '/assets/Gallery/images/IMG_1396.png',
+    '/assets/Gallery/images/IMG_1400.png',
+    '/assets/Gallery/images/IMG_1403.png',
+    '/assets/Gallery/images/IMG_1390.png',
+    '/assets/Gallery/images/IMG_1404.png',
+    '/assets/Gallery/images/IMG_1405.png',
+    '/assets/Gallery/images/IMG_1383.png',
+    '/assets/Gallery/images/IMG_1388.png',
   ]
 
   // Define width classes for each image
@@ -37,23 +57,55 @@ export function Gallery() {
     '/assets/Gallery/images/IMG_1419.png': 'w-1/2',
     '/assets/Gallery/images/IMG_1413.png': 'w-2/5',
     '/assets/Gallery/images/IMG_1414.png': 'w-2/5',
+    '/assets/Gallery/images/IMG_1389.png': 'w-2/5',
+    '/assets/Gallery/images/IMG_1391.png': 'w-2/5',
+    '/assets/Gallery/images/IMG_1396.png': 'w-2/5',
+    '/assets/Gallery/images/IMG_1400.png': 'w-2/5',
+    '/assets/Gallery/images/IMG_1403.png': 'w-2/5',
+    '/assets/Gallery/images/IMG_1390.png': 'w-2/5',
+    '/assets/Gallery/images/IMG_1404.png': 'w-2/5',
+    '/assets/Gallery/images/IMG_1405.png': 'w-2/5',
+    '/assets/Gallery/images/IMG_1383.png': 'w-2/5',
+    '/assets/Gallery/images/IMG_1388.png': 'w-2/5',
   }
 
   return (
-    <div className="embla overflow-hidden overflow-x-hidden w-4/5 mx-auto" ref={emblaRef}>
-      <div className="embla__container flex">
-        {images.map((src, index) => (
-          <div key={index} className={`embla__slide flex-none ${imageWidths[src]} px-2 mr-11 relative h-[45vh] rounded-xl overflow-hidden`}>
-            <Image 
-              src={src}
-              alt={`Gallery image ${index + 1}`}
-              fill
-              style={{ objectFit: 'cover' }}
-              sizes="(max-width: 768px) 100vw, 50vw"
-              priority={index === 0}
-            />
-          </div>
-        ))}
+    <div className="space-y-8">
+      <div className="text-center text-8xl font-extrabold text-[#383A85] p-8">
+        Galeri TIS FTUI 2025
+      </div>
+      <div className="embla overflow-hidden overflow-x-hidden w-full" ref={emblaRef1}>
+        <div className="embla__container flex">
+          {images1.map((src, index) => (
+            <div key={index} className={`embla__slide flex-none ${imageWidths[src]} mr-11 relative h-[52vh] rounded-xl overflow-hidden`}>
+              <Image 
+                src={src}
+                alt={`Gallery image ${index + 1}`}
+                fill
+                style={{ objectFit: 'cover' }}
+                sizes="(max-width: 768px) 100vw, 50vw"
+                priority={index === 0}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla overflow-hidden overflow-x-hidden w-full" ref={emblaRef2}>
+        <div className="embla__container flex">
+          {images2.map((src, index) => (
+            <div key={index} className={`embla__slide flex-none ${imageWidths[src]} mr-11 relative h-[52vh] rounded-xl overflow-hidden`}>
+              <Image 
+                src={src}
+                alt={`Gallery image ${index + 1}`}
+                fill
+                style={{ objectFit: 'cover' }}
+                sizes="(max-width: 768px) 100vw, 50vw"
+                priority={index === 0}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
