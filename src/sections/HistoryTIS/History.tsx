6c44aaae3aacb532b4d6ed76b28ce0c40ed19bf4
davidalexander2411 <davidalexander2411@gmail.com>
2025-05-21 13:15:55 +0700

    added history template

--- src/sections/HistoryTIS/History.tsx
+++ src/sections/HistoryTIS/History.tsx
@@ -1,7 +1,29 @@
+import React from 'react'
+import Image from 'next/image'
+
 const History = () => {
   return (
-    <div id="historytis" className="w-screen">
-      History
+    <div id="historytis" className="w-screen mt-12">
+      <div className="relative">
+        <Image
+          src="/assets/History/images/Group 809.png"
+          alt="History Title"
+          width={1000}
+          height={500}
+          className="ml-21 max-w-[1000px] relative z-10 -mb-[110px]"
+        />
+      </div>
+      <div className="bg-[#DCF0FA] pt-[100px]">
+        <div className="flex justify-end">
+          <Image
+            src="/assets/History/images/pensil.png"
+            alt="Pencil"
+            width={275}
+            height={0}
+            className="mr-25 relative z-10 -mt-[185px]"
+          />
+        </div>
+      </div>
     </div>
   );
 };
