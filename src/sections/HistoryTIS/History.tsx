197dfff080762fe4d0baf7086e22a769da49d1ad
davidalexander2411 <davidalexander2411@gmail.com>
2025-05-25 15:51:42 +0700

    finished history mobile

--- src/sections/HistoryTIS/History.tsx
+++ src/sections/HistoryTIS/History.tsx
@@ -3,26 +3,26 @@ import Image from 'next/image'
 
 const History = () => {
   return (
-    <div id="historytis" className="w-screen mt-12">
+    <div id="historytis" className="w-screen mt-7 md:mt-12">
       <div className="relative flex justify-center">
         <Image
           src="/assets/History/images/Group 809.png"
           alt="History Title"
           width={1000}
           height={500}
-          className="max-w-[1000px] relative z-10 -mb-[110px] mr-90"
+          className="w-68 md:w-[1000px] relative z-10 -mb-[30px] md:-mb-[110px] mr-25 md:mr-90"
         />
       </div>
 
 
-      <div className="bg-[#DCF0FA] pt-[100px] pb-25">
+      <div className="bg-[#DCF0FA] pt-[100px] pb-15 md:pb-35">
         <div className="flex flex-col items-center">
           <Image
             src="/assets/History/images/pensil.png"
             alt="Pencil"
             width={275}
             height={0}
-            className="relative z-30 -mt-[177px] ml-270"
+            className="w-20 md:w-[275px] relative z-30 -mt-[115px] md:-mt-[177px] ml-69 md:ml-270"
           />
 
 
@@ -32,13 +32,13 @@ const History = () => {
               alt="2005"
               width={180}
               height={180}
-              className="-mt-[300px] mr-245"
+              className="w-17 md:w-[180px] -mt-[100px] md:-mt-[300px] mr-58 md:mr-245"
             />
-            <div className="mr-32">
-              <div className="text-6xl font-bold text-shadow-lg text-[#383A85] -mt-[60px] text-left">
+            <div className="mr-12 md:mr-32">
+              <div className="text-xl md:text-[3.5rem] font-bold text-shadow-lg text-[#383A85] ml-20 md:ml-0 -mt-6 md:-mt-[67px] text-left">
                 Awal Mula TIS
               </div>
-              <div className="text-3xl w-[600px] text-[#383A85] mt-4 leading-relaxed text-left">
+              <div className="text-xs md:text-[2rem] w-63 md:w-[600px] text-[#383A85] mt-2.5 md:mt-0 leading-normal text-left">
                 TIS didirikan pada tanggal 4 Februari 2005 di Depok, Jawa Barat. Kampung Lio, Depok merupakan daerah operasional pertama TIS FTUI.
               </div>
             </div>
@@ -50,23 +50,23 @@ const History = () => {
             alt="garis"
             width={1000}
             height={1000}
-            className="relative z-10 -mt-[85px] ml-85"
+            className="h-12 md:h-max w-80 md:w-[1000px] relative z-10  -mt-[16px] md:-mt-[85px] ml-5 md:ml-85"
           />
           
           
-          <div className="flex flex-col items-center mt-15 ml-20">
+          <div className="flex flex-col items-center md:mt-15 mr-10 md:mr-0 md:ml-20">
             <Image
               src="/assets/History/images/2013.png"
               alt="2013"
               width={180}
               height={180}
-              className="ml-240"
+              className="w-15 md:w-[180px] ml-67 md:ml-240"
             />
-            <div className="">
-              <div className="text-6xl font-bold text-shadow-lg text-[#383A85] -mt-[60px] text-right">
+            <div className="ml-2 md:ml-0">
+              <div className="text-xl md:text-[3.5rem] font-bold text-shadow-lg text-[#383A85] mr-18 md:mr-0 -mt-[25px] md:-mt-[75px] text-right">
                 Perpustakaan Terbuka
               </div>
-              <div className="text-3xl w-[500px] text-[#383A85] mt-4 ml-50 leading-relaxed text-right">
+              <div className="text-xs md:text-[2rem] w-[320px] md:w-[500px] text-[#383A85] md:ml-50 leading-normal mt-1 md:mt-0 text-right">
                 TIS menjadikan Pondok Cina sebagai daerah operasional selanjutnya dengan salah satu fungsinya adalah sebagai perpustakaan terbuka.
               </div>
             </div>
@@ -78,23 +78,23 @@ const History = () => {
             alt="garisKiri"
             width={1000}
             height={1000}
-            className="relative z-10 -mt-[90px] mr-127"
+            className="h-8 md:h-max w-80 md:w-[1000px] relative z-10 mt-3 md:-mt-[85px] mr-23 md:mr-135"
           />
 
 
-          <div className="flex flex-col items-center mt-6">
+          <div className="flex flex-col items-center mt- md:mt-6">
             <Image
               src="/assets/History/images/2014.png"
               alt="2014"
               width={180}
               height={180}
-              className=" mr-245"
+              className="w-15 md:w-[180px] mr-59 md:mr-245"
             />
-            <div className="mr-45">
-              <div className="text-6xl font-bold text-shadow-lg text-[#383A85] -mt-[60px] text-left">
+            <div className=" md:mr-32">
+              <div className="text-xl md:text-[3.5rem] font-bold text-shadow-lg text-[#383A85] ml-20 md:ml-0 -mt-6 md:-mt-[67px] text-left">
                 Pasar Minggu
               </div>
-              <div className="text-3xl w-[550px] text-[#383A85] mt-4 leading-relaxed text-left">
+              <div className="text-xs md:text-[2rem] w-75 md:w-[600px] text-[#383A85] mt-1 md:mt-0 leading-normal text-left">
                 TIS berganti daerah operasional dengan mendirikan sebuah saung kecil di daerah Karang Pola, Pasar Minggu.
               </div>
             </div>
@@ -106,23 +106,23 @@ const History = () => {
             alt="garisKanan"
             width={1000}
             height={1000}
-            className="relative z-10 -mt-[90px] ml-127 transform scale-x-[-1]"
+            className="h-8 md:h-max w-80 md:w-[1000px] relative z-10 -mt-4 md:-mt-[90px] ml-23 md:ml-127 transform scale-x-[-1]"
           />
 
           
-          <div className="flex flex-col items-center ml-20">
+          <div className="flex flex-col items-center ml-8 md:ml-20 md:mt-0">
             <Image
               src="/assets/History/images/2015.png"
               alt="2015"
               width={180}
               height={180}
-              className="ml-240"
+              className="w-15 md:w-[180px] ml-50 md:ml-240"
             />
-            <div className="ml-15">
-              <div className="text-6xl font-bold text-shadow-lg text-[#383A85] -mt-[60px] text-right">
+            <div className="ml-10 md:ml-15">
+              <div className="text-xl md:text-[3.5rem] font-bold text-shadow-lg text-[#383A85] mr-17 md:mr-0 -mt-6 md:-mt-[72px] text-right">
                 Promosi
               </div>
-              <div className="text-3xl w-[450px] text-[#383A85] mt-4 ml-50 leading-relaxed text-right">
+              <div className="text-xs md:text-[2rem] md:w-[450px] text-[#383A85] md:ml-50 leading-normal text-right">
                 TIS menjadi Badan Otonom IKM FTUI.
               </div>
             </div>
@@ -134,23 +134,23 @@ const History = () => {
             alt="garisKiri"
             width={1000}
             height={1000}
-            className="relative z-10 -mt-[85px] mr-127"
+            className="h-8 md:h-max w-80 md:w-[1000px] relative z-10 -mt-1 md:-mt-[85px] mr-23 md:mr-135"
           />
 
 
-          <div className="flex flex-col items-center mt-6">
+          <div className="flex flex-col items-center md:mt-6">
             <Image
               src="/assets/History/images/2020.png"
               alt="2020"
               width={180}
               height={180}
-              className=" mr-245"
+              className="w-15 md:w-[180px] mr-59 md:mr-245"
             />
-            <div className="mr-58">
-              <div className="text-6xl font-bold text-shadow-lg text-[#383A85] -mt-[60px] text-left">
+            <div className="mr-25 md:mr-58">
+              <div className="text-xl md:text-[3.5rem] font-bold text-shadow-lg text-[#383A85] ml-20 md:ml-0 -mt-6 md:-mt-[70px] text-left">
                 Manara
               </div>
-              <div className="text-3xl w-[500px] text-[#383A85] mt-4 leading-relaxed text-left">
+              <div className="text-xs md:text-[2rem] w-50 md:w-[500px] mt-1 md:mt-0 text-[#383A85] leading-normal text-left">
                 Ditunjuknya Manara sebagai daerah operasional TIS yang kedua.
               </div>
             </div>
@@ -162,24 +162,24 @@ const History = () => {
             alt="garisKanan"
             width={1000}
             height={1000}
-            className="relative z-10 -mt-[90px] ml-127 transform scale-x-[-1]"
+            className="h-8 md:h-max w-80 md:w-[1000px] relative z-10 -mt-4 md:-mt-[90px] ml-23 md:ml-127 transform scale-x-[-1]"
           />
 
 
-          <div className="flex flex-col items-center ml-20">
+          <div className="flex flex-col items-center ml-8 md:ml-20 mt-1.5 md:mt-0">
             <Image
               src="/assets/History/images/2026.png"
               alt="2026"
               width={180}
               height={180}
-              className="ml-240"
+              className="w-15 md:w-[180px] ml-50 md:ml-240"
             />
-            <div className="ml-15">
-              <div className="text-6xl font-bold text-shadow-lg text-[#383A85] -mt-[60px] text-right">
+            <div className="mr-48 md:mr-0 md:ml-15">
+              <div className="text-xl md:text-[3.5rem] font-bold text-shadow-lg text-[#383A85] mr-17 md:mr-0 -mt-6 md:-mt-[70px] text-right">
                 Daops Baru
               </div>
-              <div className="text-3xl w-[450px] text-[#383A85] mt-4 ml-50 leading-relaxed text-right">
-                Daerah operasional “Baru” ditambahkan.
+              <div className="text-xs md:text-[2rem] w-[450px] text-[#383A85] md:ml-50 leading-normal text-right">
+                Daerah operasional "Baru" ditambahkan.
               </div>
             </div>
           </div>
